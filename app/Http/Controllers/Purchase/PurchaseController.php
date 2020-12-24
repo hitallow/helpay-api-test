5ac931eb2c5357ee4a3d3e9064110e5abebb19a6
hitallow <hitallo91@gmail.com>
2020-12-23 23:47:40 -0300

    faet: purchase controller and register in routes

---
+++ app/Http/Controllers/Purchase/PurchaseController.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Http\Controllers\Purchase;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Providers\Product\ProductProvider;
+use App\Providers\Purchase\PurchaseProvider;
+use Illuminate\Support\Facades\Response;
+
+class PurchaseController extends Controller
+{
+
+  private $purchaseProvider;
+  private $productProvider;
+
+  public function __construct()
+  {
+    $this->purchaseProvider = new PurchaseProvider();
+    $this->productProvider = new ProductProvider();
+  }
+  /**
+   * Executa uma nova compra
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+
+    $values = $request->all();
+    $erros = $this->purchaseProvider->validate($values);
+
+    if ($erros)
+      return Response::json([
+        'message' => 'Erro nos dados enviados',
+        'details' => $erros
+      ], 400);
+
+    return $this->purchaseProvider->save($values);
+  }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Purchase\PurchaseController;
 
 Route::apiResources([
   'products' => ProductController::class
 ]);
+
+Route::post(
+  '/purchase',
+  [PurchaseController::class, 'store']
+);
